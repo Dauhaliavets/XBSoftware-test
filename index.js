cc69732a084460a5ef424f3ae13fb8050a7c5a5e
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-06-14 01:40:33 +0300

    feat: implement save to localStorage

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-import App from "./components/App.js";
-import AppState from "./components/AppState.js";
+import App from './components/App.js';
+import AppState from './components/AppState.js';
 
-console.log("Hello XB Software")
+console.log('Hello XB Software');
 
 const defaultState = {
   inputValue: '',
   tags: [],
   mode: false,
 };
-
 const container = document.getElementById('container');
 
-const state = new AppState(defaultState);
-const app = new App(container, state);
+window.onload = () => {
+  const state = JSON.parse(localStorage.getItem('savedState'));
+
+  const appState = new AppState(state || defaultState);
+  new App(container, appState);
+
+  window.onbeforeunload = () => {
+    localStorage.setItem('savedState', JSON.stringify(appState.data));
+  };
+};
