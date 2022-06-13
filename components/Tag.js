96e2dedefd9c3610f574b0aff87733b147881e2b
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-06-14 02:19:19 +0300

    feat: add styles

--- components/Tag.js
+++ components/Tag.js
@@ -4,6 +4,10 @@ class Tag extends Control {
   constructor(parent, content, mode, handler) {
     super(parent, 'li', 'tag-item', content);
 
+    if(mode) {
+      this.node.classList.add('tag-item_disabled')
+    }
+
     this.btnDelete = new Control(this.node, 'span', 'button-delete', 'X');
     this.btnDelete.node.addEventListener('click', () => {
       if(!mode) {
