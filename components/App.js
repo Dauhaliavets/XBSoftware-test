97d7450886f338573047f5a29e8dd215810e2b24
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-06-14 17:14:00 +0300

    refactor: minor refactor App, SwitchMode

--- components/App.js
+++ components/App.js
@@ -5,25 +5,14 @@ import SwitchMode from './SwitchMode.js';
 import TagsList from './TagsList.js';
 
 class App extends Control {
-	constructor(parent, state) {
-		super(parent, 'div', 'app');
+  constructor(parent, state) {
+    super(parent, 'div', 'app');
 
-		const update = (data) => {
-			this.node.innerHTML = '';
-			new InputElement(this.node, state);
-			new ButtonAdd(this.node, state);
-			new SwitchMode(this.node, data.mode, () => this.changeMode(state));
-			new TagsList(this.node, state);
-		};
-
-		state.onChange.add(update);
-		update(state.data);
-	}
-
-	changeMode = (state) => {
-		const newMode = !state.data.mode;
-		state.data = { ...state.data, mode: newMode };
-	};
+    new InputElement(this.node, state);
+    new ButtonAdd(this.node, state);
+    new SwitchMode(this.node, state);
+    new TagsList(this.node, state);
+  }
 }
 
 export default App;
--- components/SwitchMode.js
+++ components/SwitchMode.js
@@ -1,17 +1,26 @@
-import Control from "./common/Control.js";
+import Control from './common/Control.js';
 
 class SwitchMode extends Control {
-  constructor(parent, mode, callback) {
+  constructor(parent, state) {
     super(parent, 'input', 'checkbox-mode');
-    this.node.setAttribute('type', 'checkbox');
 
-    if(mode) {
+    this.node.setAttribute('type', 'checkbox');
+    this.node.addEventListener('input', () => this.changeMode(state));
+    state.onChange.add(this.update);
+    this.update(state.data);
+  }
+
+  changeMode = (state) => {
+    const newMode = !state.data.mode;
+    state.data = { ...state.data, mode: newMode };
+  };
+
+  update = (data) => {
+    if (data.mode) {
       this.node.checked = true;
     } else {
       this.node.checked = false;
     }
-
-    this.node.addEventListener('input', () => callback())
   }
 }
 
